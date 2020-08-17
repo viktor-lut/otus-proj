5aae9f83968a28c6595e3e40a9819f2000a14b5b
viktor-lut <lvihard@gmail.com>
2020-08-17 08:49:32 +0300

    home work #2 (folder 'codecept_homework_2')

--- codecept_homework_2/addissout_test.js
+++ codecept_homework_2/addissout_test.js
@@ -1,29 +1,44 @@
+const fs = require('fs');
 Feature('home work 2-2');
 
-Scenario('add issue from out text', async (I) => {
-
-  I.amOnPage('https://github.com/minimaxir/big-list-of-naughty-strings/blob/master/blns.txt');
-  let currtxt = await I.grabTextFrom('tbody');
-  I.amOnPage('/examples/emberjs/');
-  I.fillField('#new-todo', currtxt);
-  I.pressKey('Enter');
-  I.see(currtxt);
-})
-
-
-
-// Scenario('add issue', (I) => {
+// Scenario('add issue from out text', async (I) => {
 //
 //   I.amOnPage('https://github.com/minimaxir/big-list-of-naughty-strings/blob/master/blns.txt');
-//   const text = new DataTable(['rowtext']);
-//   let currtxt = I.grabTextFrom('tbody');
-//   text.add([currtxt]);
-//
-//   Data(text).Scenario('add issue from out text', (I, current) => {
-//     const {rowtext} = current;
-//     I.amOnPage('/examples/emberjs/');
-//     I.fillField('#new-todo', rowtext);
-//     I.pressKey('Enter');
-//     I.see(txt, 'label');
-//   })
+//   let currtxt = await I.grabTextFrom('tbody');
+//   I.amOnPage('/examples/emberjs/');
+//   I.fillField('#new-todo', currtxt);
+//   I.pressKey('Enter');
+//   I.see(currtxt);
 // })
+
+Feature('issue');
+
+const textFile = fs.readFileSync(__dirname + '/words.txt').toString();
+const credentials = textFile.split('\r\n');
+
+// const text = new DataTable(['rowtext']);
+const text = [];
+let count = 0;
+
+credentials.forEach(line => {
+  if (!line) return;
+  // const [currtxt] = line.split(':');
+  // const [currtxt] = line;
+  // text.add([currtxt]);
+  text.push(line);
+  count++;
+})
+
+;
+
+Data(text).Scenario('Add issue from out text', async (I, current) => {
+
+    I.amOnPage('/examples/emberjs/');
+    I.fillField('#new-todo', current);
+
+    I.pressKey('Enter');
+
+    let txt = '22' + ' items left';
+    I.see(txt, '#todo-count');
+
+})
