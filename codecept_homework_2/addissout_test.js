9cddc148f897de2cb76781f920e75448ff95085b
viktor-lut <lvihard@gmail.com>
2020-08-17 08:50:44 +0300

    home work #2 (folder 'codecept_homework_2')

--- codecept_homework_2/addissout_test.js
+++ codecept_homework_2/addissout_test.js
@@ -1,32 +1,17 @@
 const fs = require('fs');
 Feature('home work 2-2');
 
-// Scenario('add issue from out text', async (I) => {
-//
-//   I.amOnPage('https://github.com/minimaxir/big-list-of-naughty-strings/blob/master/blns.txt');
-//   let currtxt = await I.grabTextFrom('tbody');
-//   I.amOnPage('/examples/emberjs/');
-//   I.fillField('#new-todo', currtxt);
-//   I.pressKey('Enter');
-//   I.see(currtxt);
-// })
-
 Feature('issue');
 
 const textFile = fs.readFileSync(__dirname + '/words.txt').toString();
 const credentials = textFile.split('\r\n');
 
-// const text = new DataTable(['rowtext']);
 const text = [];
 let count = 0;
 
 credentials.forEach(line => {
   if (!line) return;
-  // const [currtxt] = line.split(':');
-  // const [currtxt] = line;
-  // text.add([currtxt]);
   text.push(line);
-  count++;
 })
 
 ;
